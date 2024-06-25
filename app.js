d446e298ff3cfa0d0726ce24d9367cbd82784831
=pranav <=pranavkewate7820@gmail.com>
2024-06-25 11:01:13 +0530

    update the event of cross & menu btn

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-function showSidebar(){
-    const sidebar=document.querySelector('.sidebar');
-    sidebar.style.display='flex'
-}
 
-function hideSidebar(){
-    const sidebar=document.querySelector('.sidebar');
-    sidebar.style.display='none'
-}
+const sidebar = document.querySelector('.sidebar');
+
+const cross_btn = document.querySelector('.cross-btn');
+const menu_btn = document.querySelector('.menu-btn');
+
+
+cross_btn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    sidebar.style.display = 'none'
+})
+
+menu_btn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    sidebar.style.display = 'block'
+})
